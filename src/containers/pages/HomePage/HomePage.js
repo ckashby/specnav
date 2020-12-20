143f69e51b648ec8278d54383a4a51a2dd769762
ckashby <clay.ashby@gmail.com>
2020-12-20 02:31:22 -0800

    setup page navigation

--- src/containers/pages/HomePage/HomePage.js
+++ src/containers/pages/HomePage/HomePage.js
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Text, ToggleButton } from "@adobe/react-spectrum";
+import Airplane from "@spectrum-icons/workflow/Airplane";
+import { ActionButton } from "@adobe/react-spectrum";
 
-function HomePage() {
-  return (
-    <>
-      <h2>Welcome</h2>
-      <ul>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact Us</Link>
-        </li>
-        <li>
-          <Link to="/posts">Posts</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-    </>
-  );
+class HomePage extends Component {
+  render() {
+    return (
+      <>
+        <h2>Hello</h2>
+        {/* {loading ? <h3>Loading...</h3> : <h3>Hello {showName && name}</h3>} */}
+        <ul>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </>
+    );
+  }
 }
 
 export default HomePage;
